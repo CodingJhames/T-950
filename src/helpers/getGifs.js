bf549ae1868ae8fdb596b7593cf2a48e0662edda
codingjhames <jhames.mejia@unipamplona.edu.co>
2022-06-03 15:07:12 -0500

    updated complete folder

---
+++ src/helpers/getGifs.js
@@ -0,0 +1,23 @@
+
+
+
+    export const getGifs = async ( category ) => {
+
+        const url =`https://api.giphy.com/v1/gifs/search?limit=10&q=${encodeURI(category)}&api_key=BsW6v8H74iBT4mTYmspQQnvmzK5DaDQ6`
+
+        const resp = await fetch( url);
+
+        const {data} = await resp.json();
+
+        const gifs = data.map( img => {
+            return {
+                id: img.id,
+                title: img.title,
+                url: img.images.downsized_medium.url
+            }
+
+        })
+
+        return gifs;
+        // setImages( gifs);
+    }
--- src/index.js
+++ src/index.js
@@ -1,29 +1,13 @@
-
-
-// import ReactDOM from 'react-dom'
-
-
-// const divRoot = document.querySelector('#root');
-
-// const saludo = <h1> Hola Chepito</h1>
-
-// ReactDOM.render( saludo, divRoot);
-
-// console.log("Hola Micho");
-// console.log(divRoot);
-
+import React from 'react';
 import { createRoot } from 'react-dom/client';
-// import PrimeraApp from './PrimeraApp';
-import CounterApp from './CounterApp';
-import './index.css'
+import GifExpertApp from './GifExpertApp';
+import './index.css';
 
 
 
-const saludo = <h1> Hola Micho</h1>
+const container = document.getElementById('root');
+const root = createRoot(container); // createRoot(container!) if you use TypeScript
 
-const divRoot = document.querySelector('#root');
-const root = createRoot(divRoot);
+root.render(<GifExpertApp  />);
 
-// root.render( < PrimeraApp saludo="Hey Arnold!" />);
-root.render( < CounterApp value={21} />);
 
