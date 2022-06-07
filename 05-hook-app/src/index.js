bafa8f75c25068c8604a8f6dc34c269c8b2bf300
codingjhames <jhames.mejia@unipamplona.edu.co>
2022-06-06 21:49:46 -0500

    updated 05-folder

--- 05-hook-app/src/index.js
+++ 05-hook-app/src/index.js
@@ -4,12 +4,16 @@ import { createRoot } from 'react-dom/client';
 // import { CounterApp } from './components/01-useState/CounterApp';
 // import { CounterWithCustomHook } from './components/01-useState/CounterWithCustomHook';
 // import SimpleForm from './components/02-useEffect/SimpleForm';
-import {MultipleCustomHooks} from './components/03-examples/MultipleCustomHooks';
+// import {MultipleCustomHooks} from './components/03-examples/MultipleCustomHooks';
+// import { FocusScreen } from './components/04-useRef/FocusScreen';
+// import RealEXampleRef from './components/04-useRef/RealEXampleRef';
+// import { Layout } from './components/05-useLayoutEffect/LayoutEffect';
+import Memorize from './components/06-memos/Memorize';
 
 
 const container = document.getElementById('root');
 const root = createRoot(container); // createRoot(container!) if you use TypeScript
-root.render( < MultipleCustomHooks  /> );
+root.render( < Memorize /> );
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
