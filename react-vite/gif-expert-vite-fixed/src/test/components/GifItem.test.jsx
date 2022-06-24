71afe7ed0977ac6f57357e83e90401874fd4bb0a
codingjhames <jhames.mejia@unipamplona.edu.co>
2022-06-24 11:37:30 -0500

    updated tests

--- react-vite/gif-expert-vite-fixed/src/test/components/GifItem.test.jsx
+++ react-vite/gif-expert-vite-fixed/src/test/components/GifItem.test.jsx
@@ -1,5 +1,5 @@
 
-import { render } from "@testing-library/react";
+import { render, screen } from "@testing-library/react";
 import { GifItem } from "../../components/GifItem";
 
 describe('Pruebas en GifItem', () => { 
@@ -9,10 +9,35 @@ describe('Pruebas en GifItem', () => {
 
     test('Debe de hacer match con el snapshot', () => { 
 
-       const {container} = render( < GifItem title={title} url={url} />)
+       const {container} = render( < GifItem title={title} url={url} />);
        expect( container ).toMatchSnapshot();
 
      }) 
 
+
+     test('Debe de mostrar la imagen con el URL y el Alt indicado', () => { 
+
+      render( < GifItem title={title} url={url} />);
+      // screen.debug();
+
+      // expect( screen.getByRole('img').src).toBe( url );
+      // expect( screen.getByRole('img').alt).toBe( title );
+
+      /* aqui fernando sugiere que seguir esa manera de una en una sería muy
+      larga y acumularía muchos expects para estar evaluando pruebas */
+
+      const { src, alt  } =  screen.getByRole('img');
+      expect( src ).toBe( url );
+      expect( alt ).toBe( alt );
+
+      })
+
+      test('Debe de mostrar el título en el componente', () => { 
+
+        render( < GifItem title={title} url={url} />);
+
+        expect( screen.getAllByText( title) ).toBeTruthy();
+
+       });
 
  });
