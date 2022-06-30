c17d2098aa25bfca4869a7f789f29ccb61e7ab4d
codingjhames <jhames.mejia@unipamplona.edu.co>
2022-06-30 10:10:31 -0500

    test last version

--- gif-exp-f/src/test/components/GifGrid.test.jsx
+++ gif-exp-f/src/test/components/GifGrid.test.jsx
@@ -1,5 +1,8 @@
 import { render, screen } from "@testing-library/react";
 import GifGrid from "../../components/GifGrid";
+import { useFetchGifs } from "../../hooks/useFetchGifs";
+
+jest.mock("../../hooks/useFetchGifs");
 
 
 describe('Pruebas en GifGrid', () => { 
@@ -7,6 +10,11 @@ describe('Pruebas en GifGrid', () => {
     const category = 'Demon Slayer';
 
     test('Debe de mostrar el Loading inicialmente', () => { 
+
+        useFetchGifs.mockReturnValue({
+            images: [],
+            isLoading: true
+        })
 
         render( < GifGrid  category={category} /> );
         // screen.debug();
@@ -16,9 +24,32 @@ describe('Pruebas en GifGrid', () => {
 
      test('Debe de mostrar items cuando se cargan las imagenes de useFecthGifs', () => { 
 
-        
+        const Gifs = [
+            {
+                id: 'ABC',
+                title: 'Demon Slayer',
+                url: 'https://localhost/Demon-Slayer.jpg'
+            },
+            {
+                id: '123',
+                title: 'Goku',
+                url: 'https://localhost/Goku.jpg'
+            },
 
-      })
+        ]
 
+        useFetchGifs.mockReturnValue({
+            images: Gifs,
+            isLoading: false
+        });
+
+        render( < GifGrid  category={category} /> );
+        // screen.debug();
+
+        expect( screen.getAllByRole( 'img').length ).toBe(2);
+
+      });
+
+      
 
  })
