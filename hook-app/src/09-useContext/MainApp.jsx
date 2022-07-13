b2a5049b327728a434782f1794f918bd14eb0d77
codingjhames <jhames.mejia@unipamplona.edu.co>
2022-07-13 12:29:43 -0500

    begin React Router Routes

--- hook-app/src/09-useContext/MainApp.jsx
+++ hook-app/src/09-useContext/MainApp.jsx
@@ -1,13 +1,27 @@
+import { Navigate, Route, Routes } from "react-router-dom"
+import AboutPage from "./AboutPage"
+import HomePage from "./Homepage"
+import LoginPage from "./LoginPage"
 
 
 const MainApp = () => {
 
-    
   return (
     <>
         <h1>MainApp</h1>
         <hr></hr>
-    
+
+        <Routes>
+            <Route path="/" element={ <HomePage /> }/>
+            <Route path="login" element={ <LoginPage /> }/>
+            <Route path="about" element={ <AboutPage /> }/>
+            {/* <Route path="/*" element={ <HomePage /> }/> */}
+            <Route path="/*" element={ <Navigate to="/about" /> }/>
+
+
+
+
+        </Routes>
     </>
   )
 }
