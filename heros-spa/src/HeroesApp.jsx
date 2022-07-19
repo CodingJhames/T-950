4c5522a4452832df3947048a1b588626b6fddb22
codingjhames <jhames.mejia@unipamplona.edu.co>
2022-07-19 11:44:08 -0500

    started new section

--- heros-spa/src/HeroesApp.jsx
+++ heros-spa/src/HeroesApp.jsx
@@ -1,10 +1,13 @@
 
 import React from 'react'
+import AppRouter from './router/AppRouter'
 
 const HeroesApp = () => {
   return (
     <>
-        <h1>Heroes App</h1>
+        <hr></hr>
+        <AppRouter/>
+        <hr></hr>
     
     </>
   )
