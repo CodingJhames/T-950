3d62f92f32c5efae6f3a953ac89d916dd49e9cdd
codingjhames <jhames.mejia@unipamplona.edu.co>
2022-07-20 17:19:30 -0500

    updated Dc & Marvel Pages

--- heros-spa/src/auth/components/HeroCard.jsx
+++ heros-spa/src/auth/components/HeroCard.jsx
@@ -1,6 +1,7 @@
 
 
 import React from 'react'
+import { Link } from 'react-router-dom';
 
 const HeroCard = ( {
         id,
@@ -12,6 +13,20 @@ const HeroCard = ( {
 }) => {
 
     const heroImageUrl = `/assets/heroes/${id}.jpg`;
+
+    const charactersByHero = ( <p>{characters}</p> );
+
+    /* otra alternativa que sugiere fernando es crear un componente
+    que sólo se use aquí, en razón que según él es más fácil de leer. 
+
+    const charactersByHero = ( { alter_ego, characters  } ) => {
+
+        if ( alter_ego === characters ) return (<></>);
+            
+        return <p>{characters}</p>; 
+        
+    } */
+
 
   return (
     <div className='col'>
@@ -29,7 +44,42 @@ const HeroCard = ( {
 
                     <h5 className='card-title'>{ superhero}</h5>
                     <p className='card-text'>{alter_ego}</p>
-                    <p>{characters}</p>
+                    
+                    {/* primera forma para que no se muestren 2 veces el nombre del personaje */}
+
+                    {/* {
+                        ( alter_ego !== characters ) && ( <p>{characters}</p> )
+                    } */}
+
+                    {/* segunda forma que sugiere fernando Herrera */}
+
+                    {
+                        ( alter_ego !== characters ) && charactersByHero 
+                    }
+
+                    {/* 
+                    
+                    { 
+                        ( alter_ego !== characters ) && <p> { characters } </p>
+                    }
+                    
+                    */}
+
+
+                    {/* Forma alterna que sugiere fernando a través de un componente
+                    
+                    <charactersByHero  characters={ characters } alter_ego={ alter_ego } />
+
+                    */}
+
+
+                    <p className='card-text'>
+                        <small className='text-muted'>{first_appearance}</small>
+                    </p>
+
+                    <Link to={`/hero/${id}`}>
+                        Mas..
+                    </Link>
 
 
                 </div>
