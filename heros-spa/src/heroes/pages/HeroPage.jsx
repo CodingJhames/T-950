e7cdfc051ec7812d28fcb28b0d1f2b53e071c50e
codingjhames <jhames.mejia@unipamplona.edu.co>
2022-07-20 18:18:47 -0500

    query params and useparams

--- heros-spa/src/heroes/pages/HeroPage.jsx
+++ heros-spa/src/heroes/pages/HeroPage.jsx
@@ -1,6 +1,21 @@
+import { Navigate, useParams } from "react-router-dom";
+import getHeroById from "../helpers/getHeroById";
 
 
 const HeroPage = () => {
+
+  const { id } = useParams();
+  // console.log( id );
+
+  const hero = getHeroById( id );
+  // console.log( hero );
+
+  if ( !hero ) {
+    
+    return <Navigate to="/marvel" />
+  }
+
+
   return (
     <h1>HeroPage</h1>
   )
