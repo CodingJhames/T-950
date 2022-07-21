8c68b13b9d98aa4958d25a6a1db8a00ad5b49fdf
codingjhames <jhames.mejia@unipamplona.edu.co>
2022-07-21 11:09:09 -0500

    updated heroPage

--- heros-spa/src/heroes/pages/HeroPage.jsx
+++ heros-spa/src/heroes/pages/HeroPage.jsx
@@ -1,4 +1,5 @@
-import { Navigate, useParams } from "react-router-dom";
+import { Navigate, useNavigate, useParams } from "react-router-dom";
+import { heroes } from "../data/heroes";
 import getHeroById from "../helpers/getHeroById";
 
 
@@ -10,6 +11,14 @@ const HeroPage = () => {
   const hero = getHeroById( id );
   // console.log( hero );
 
+  const navigate = useNavigate();
+
+  const onNavigateBack = () => {
+
+    navigate( -1 );
+      
+  }
+
   if ( !hero ) {
     
     return <Navigate to="/marvel" />
@@ -17,7 +26,37 @@ const HeroPage = () => {
 
 
   return (
-    <h1>HeroPage</h1>
+
+    <div className="row mt-5">
+      <div className="col-4">
+        <img
+        src={`/assets/heroes/${id }.jpg`}
+        alt={ hero.superhero }
+        className="img-thumbnail"></img>
+      </div>
+
+      <div className="col-8">
+          <h3>{hero.superhero}</h3>
+          <ul className="list-group list-group-flush">
+            <li className="list-group-item"> <b>Alter ego: { hero.alter_ego }</b></li>
+            <li className="list-group-item"> <b>Publisher: { hero.publisher }</b></li>
+            <li className="list-group-item"> <b>First Appareance: { hero.first_appearance }</b></li>
+          </ul>
+
+          <h5 className="mt-3"> Characters </h5>
+          <p>{ hero.characters }</p>
+
+          <button 
+          className="btn btn-primary"
+          onClick={ onNavigateBack }>Back</button>
+
+      </div>
+
+      
+    </div>
+
+  
+    
   )
 }
 
