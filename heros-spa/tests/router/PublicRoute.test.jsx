779293369e332b1fa070f8de2d2d8cefdd6cdbee
codingjhames <jhames.mejia@unipamplona.edu.co>
2022-07-29 15:37:45 -0500

    publicroute tests 2

--- heros-spa/tests/router/PublicRoute.test.jsx
+++ heros-spa/tests/router/PublicRoute.test.jsx
@@ -1,4 +1,5 @@
 import { render, screen } from "@testing-library/react"
+import { MemoryRouter, Route, Routes } from "react-router-dom";
 import { AuthContext } from "../../src/auth/context"
 import PublicRoute from "../../src/router/PublicRoute"
 
@@ -23,9 +24,38 @@ describe('Pruebas es PublicRoute', () => {
 
     test('Debe de navegar ni está autenticado', () => { 
 
-        
+        const contextvalue = {
+            logged: true,
+            user: {
+                name: 'Micho',
+                id: 'ABC123'
+            }
+        }
 
+        render( 
+        < AuthContext.Provider value={ contextvalue }>
+            <MemoryRouter initialEntries={['/login']}>
+
+                <Routes>
+
+                    <Route path='login' element={
+                        < PublicRoute>
+                            <h1>Ruta Publica</h1>
+                        </PublicRoute>
+                    } />
+
+                    <Route path='marvel' element={ <h1>Página Marvel</h1>} />
+
+                </Routes>
+
+            </MemoryRouter>
+            
+        </AuthContext.Provider>  )
+
+        expect(  screen.getByText( 'Página Marvel' ) ).toBeTruthy();
 
         })
 
 });
+
+
