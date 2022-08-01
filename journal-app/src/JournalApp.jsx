f11987ddf505761857ccf9a45eb7e1f12a25098d
codingjhames <jhames.mejia@unipamplona.edu.co>
2022-08-01 16:34:40 -0500

    router and routes created

--- journal-app/src/JournalApp.jsx
+++ journal-app/src/JournalApp.jsx
@@ -1,12 +1,11 @@
 
+import { AppRouter } from "./router/AppRouter"
 
 
 export const JournalApp = () => {
   return (
     <>
-      
-        <h1>hey Chepito</h1>
-    
+            <AppRouter/>
     </>
   )
 }
