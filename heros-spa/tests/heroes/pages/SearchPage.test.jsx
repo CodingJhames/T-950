8de955c23383b017b596b1bfe5ed3be451bafe80
codingjhames <jhames.mejia@unipamplona.edu.co>
2022-08-01 12:07:37 -0500

    SearchPage tests

---
+++ heros-spa/tests/heroes/pages/SearchPage.test.jsx
@@ -0,0 +1,24 @@
+import { render, screen } from "@testing-library/react"
+import { MemoryRouter } from "react-router-dom"
+import SearchPage from "../../../src/heroes/pages/SearchPage"
+
+
+describe('Pruebas en SearchPage', () => { 
+
+    test('Debe de mostrarse correctamente con valores por defecto', () => {
+
+        const { container } = render( 
+            <MemoryRouter>
+                <SearchPage/>
+            </MemoryRouter>
+            );
+
+            // screen.debug();
+
+            expect( container ).toMatchSnapshot();
+
+        })
+
+
+
+    })
