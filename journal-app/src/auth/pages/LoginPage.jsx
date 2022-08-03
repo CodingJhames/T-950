8381034ffae1775e7f376c2912fa8901f6bbfa9e
codingjhames <jhames.mejia@unipamplona.edu.co>
2022-08-03 09:05:35 -0500

    Login Buttons added

--- journal-app/src/auth/pages/LoginPage.jsx
+++ journal-app/src/auth/pages/LoginPage.jsx
@@ -1,8 +1,13 @@
 
-import { Grid, TextField, Typography } from '@mui/material'
+import { Google } from '@mui/icons-material';
+import { Link as RouterLink } from 'react-router-dom';
+import { Button, Grid, Link, TextField, Typography } from '@mui/material';
 
 
 export const LoginPage = () => {
+
+  
+
   return (
     
     <Grid 
@@ -36,7 +41,7 @@ export const LoginPage = () => {
 
             </Grid>
 
-            <Grid item xs={12} sx={{ mt: 2 }}>
+            <Grid item xs={12} sx={{ mt: 2, mt: 1 }}>
               
               <TextField 
               label='Contraseña' 
@@ -47,7 +52,34 @@ export const LoginPage = () => {
 
             </Grid>
 
+            <Grid container spacing={ 2 } sx={{ mb: 2, mt: 1 }} >
+              <Grid item xs={12} sm={6}>
 
+                <Button variant='contained' fullWidth >Login</Button>
+
+              </Grid>
+
+              <Grid item xs={12} sm={6}>
+
+                <Button variant='contained' fullWidth >   
+                
+                  <Google/> 
+                  <Typography sx={{ ml: 1}} > Google </Typography>
+
+                </Button>
+
+              </Grid>
+
+            </Grid>
+
+            <Grid container direction='row' justifyContent='end'>
+                
+                <Link component={ RouterLink }  color='inherit' to='/auth/register' >
+                  Crear una cuenta
+                </Link>
+                
+                
+            </Grid>
 
           </Grid>
 
