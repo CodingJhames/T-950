d50355c7a12e446ad145599074b3df826226908a
codingjhames <jhames.mejia@unipamplona.edu.co>
2022-08-03 13:07:26 -0500

    Login journallayout added

---
+++ journal-app/src/journal/layout/JournalLayout.jsx
@@ -0,0 +1,27 @@
+import { Box } from "@mui/system"
+
+
+export const JournalLayout = ({ children }  ) => {
+    return (
+        
+        <Box sx={ { display: 'flex' }} >
+
+            {/* navbar */}
+
+            {/* sidebar */}
+
+            <Box 
+            component='main'
+            sx={{ flexGrow: 1, p: 3 }} 
+            >
+                {/* Toolbar */}
+
+                { children }
+
+            </Box>
+
+        </Box>
+
+
+    )
+}
--- journal-app/src/journal/pages/JournalPage.jsx
+++ journal-app/src/journal/pages/JournalPage.jsx
@@ -1,15 +1,23 @@
 
 
-import { MailOutline } from '@mui/icons-material'
+
 import { Typography } from '@mui/material'
-import React from 'react'
+import { JournalLayout } from '../layout/JournalLayout'
 
 export const JournalPage = () => {
   return (
 
     <>
-      <Typography variant='h1' >JournalPage</Typography>
-      <MailOutline />
+      <JournalLayout >
+
+        <Typography  > Lorem ipsum, dolor sit amet consectetur adipisicing elit. 
+          Voluptas dolores molestias asperiores reprehenderit unde illum maxime cumque laboriosam! 
+          Nulla repellendus molestiae ut laboriosam corporis accusamus facilis tempore ad quos recusandae. 
+        </Typography>
+      
+
+      </JournalLayout>
+      
     </>
 
     
