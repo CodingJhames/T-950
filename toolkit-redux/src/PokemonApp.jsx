3dd4f97af7150d631f94db490131ac1efac958c3
codingjhames <jhames.mejia@unipamplona.edu.co>
2022-08-08 12:56:16 -0500

    reduxslice snippet added and pokemonslice

---
+++ toolkit-redux/src/PokemonApp.jsx
@@ -0,0 +1,19 @@
+
+
+import React from 'react'
+
+export const PokemonApp = () => {
+  return (
+    <>
+        <h1>PokemonApp</h1>
+        <hr></hr>
+
+        <ul>
+            <li>item</li>
+            <li>item</li>
+            <li>item</li>
+        </ul>
+    
+    </>
+  )
+}
