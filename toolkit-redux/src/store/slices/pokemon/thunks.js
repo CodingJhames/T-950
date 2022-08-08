2ec5acdba331588edb620c3e601ff0bedec9cd0e
codingjhames <jhames.mejia@unipamplona.edu.co>
2022-08-08 14:12:02 -0500

    axios method added

--- toolkit-redux/src/store/slices/pokemon/thunks.js
+++ toolkit-redux/src/store/slices/pokemon/thunks.js
@@ -1,3 +1,4 @@
+import { pokemonApi } from "../../../api/pokemonApi";
 import { setPokemons, startLoadingPokemons } from "./pokemonSlice"
 
 // ? un thunk es una función que regresa una tarea asincrona
@@ -5,13 +6,20 @@ import { setPokemons, startLoadingPokemons } from "./pokemonSlice"
 
 export const getPokemons = ( page = 0  ) => {
 
-    return ( dispatch, getState ) => {
+    return async ( dispatch, getState ) => {
         dispatch( startLoadingPokemons() );
+
         //  Todo: realizar peticiones http
+        // const resp = await fetch(`https://pokeapi.co/api/v2/pokemon?limit=10&offset=${ page * 10 }`)
+        // const data = await resp.json();
+        // console.log(data);
 
+        // ? método con Axios
 
+        const {  data } = await pokemonApi.get(`/pokemon?limit=10&offset=${ page * 10 }`);
+        console.log( data );
 
-        // dispatch( setPokemons()  );
+        dispatch( setPokemons( { pokemons: data.results, page: page + 1  } )  );
 
     }
 
