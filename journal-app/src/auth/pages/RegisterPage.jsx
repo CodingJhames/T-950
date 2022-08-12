2c04a03888ae5baa5c230068d4861c90bdc22d4b
codingjhames <jhames.mejia@unipamplona.edu.co>
2022-08-12 12:41:38 -0500

    show error message from authentication

--- journal-app/src/auth/pages/RegisterPage.jsx
+++ journal-app/src/auth/pages/RegisterPage.jsx
@@ -1,11 +1,11 @@
 
 // import { Google } from '@mui/icons-material';
 import { Link as RouterLink } from 'react-router-dom';
-import { Button, Grid, Link, TextField, Typography } from '@mui/material';
+import { Alert, Button, Grid, Link, TextField, Typography } from '@mui/material';
 import { AuthLayout } from '../layout/AuthLayout';
 import { useForm } from '../../hooks/useForm';
-import { useState } from 'react';
-import { useDispatch } from 'react-redux';
+import { useMemo, useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 import { startCreatingUserWithEmailPassword } from '../../store/auth';
 
 const formData = {
@@ -23,7 +23,11 @@ const formValidations = {
 export const RegisterPage = () => {
 
   const dispatch = useDispatch();
-  const [formSubmitted, setFormSubmitted] = useState(false)
+  const [formSubmitted, setFormSubmitted] = useState(false);
+
+  const { status, errorMessage } = useSelector( state => state.auth );
+  const isCheckingAuthentication = useMemo(() => status === 'cheking', [ status ]);
+
 
 
   const { 
@@ -105,9 +109,20 @@ export const RegisterPage = () => {
   </Grid>
 
   <Grid container spacing={ 2 } sx={{ mb: 2, mt: 1 }} >
+
+  <Grid 
+    item 
+    xs={12} 
+    display={ errorMessage ? '': 'none' }
+    >
+    <Alert severity='error'>{ errorMessage }</Alert>
+  </Grid>
+
+
     <Grid item xs={12} >
 
-      <Button 
+      <Button
+      disabled={ isCheckingAuthentication } 
       type='submit'
       variant='contained' 
       fullWidth >Crear cuenta</Button>
