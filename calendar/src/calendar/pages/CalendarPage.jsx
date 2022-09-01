8f6709187273235bc9a6a555ee938ceae433b5e7
codingjhames <jhames.mejia@unipamplona.edu.co>
2022-08-31 20:00:25 -0500

    aditional configs to calendar

--- calendar/src/calendar/pages/CalendarPage.jsx
+++ calendar/src/calendar/pages/CalendarPage.jsx
@@ -1,21 +1,26 @@
-import { Calendar, dateFnsLocalizer } from 'react-big-calendar';
+import { Calendar } from 'react-big-calendar';
 import 'react-big-calendar/lib/css/react-big-calendar.css';
-import enUS from 'date-fns/locale/en-US';
 
-import { NavBar } from "../";
-import { addHours,parse,startOfWeek,getDay,format } from 'date-fns'
+import { addHours } from 'date-fns';
+import { NavBar } from '../';
+import { localizer, getMessagesES } from '../../helpers';
+// import enUS from 'date-fns/locale/en-US';
 
-const locales = {
-  'en-US': enUS,
-}
+// import { NavBar } from "../";
+// import { addHours,parse,startOfWeek,getDay,format } from 'date-fns'
 
-const localizer = dateFnsLocalizer({
-  format,
-  parse,
-  startOfWeek,
-  getDay,
-  locales,
-})
+// const locales = {
+//   'en-US': enUS,
+// }
+
+// const localizer = dateFnsLocalizer({
+//   format,
+//   parse,
+//   startOfWeek,
+//   getDay,
+//   locales,
+// })
+
 
 const events = [{
   title: 'Cumple Parchita',
@@ -30,16 +35,38 @@ const events = [{
 }];
 
 export const CalendarPage = () => {
+
+  const eventStyleGetter = ( event, start, end, isSelected ) => {
+    console.log( event, start, end, isSelected );
+
+    const style = {
+      backgroundColor: '#347CF7',
+      boderRadius: '0px',
+      opacity: 0.8,
+      color: 'white',
+    }
+
+    return {
+      style
+    }
+
+  }
+
+
+
   return (
     <>
       <NavBar />
 
       <Calendar
+        culture='es'
         localizer={localizer}
         events={events}
         startAccessor="start"
         endAccessor="end"
         style={{ height: 'calc(100vh - 80px)' }}
+        messages={getMessagesES()}
+        eventPropGetter={ eventStyleGetter }
       />
 
 
