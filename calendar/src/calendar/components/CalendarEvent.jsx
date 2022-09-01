e3f6cc18bb639d629af06eb62cd1bd0c8073e54c
codingjhames <jhames.mejia@unipamplona.edu.co>
2022-09-01 10:18:00 -0500

    event frame custom

---
+++ calendar/src/calendar/components/CalendarEvent.jsx
@@ -0,0 +1,17 @@
+
+
+export const CalendarEvent = ({event}) => {
+
+    const {title, user } = event;
+    // console.log( props );
+
+
+  return (
+    <>
+        <strong>{title}</strong>
+        <span>- { user.name }</span>
+    
+    
+    </>
+  )
+}
