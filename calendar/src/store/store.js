bbedefacb9d9af12365ac1a9233d562beccc614a
codingjhames <jhames.mejia@unipamplona.edu.co>
2022-09-03 11:13:32 -0500

    authslice & store added

---
+++ calendar/src/store/store.js
@@ -0,0 +1,31 @@
+import { configureStore } from "@reduxjs/toolkit";
+import { uiSlice } from "./ui/uiSlice";
+
+export const store = configureStore({
+
+    reducer: {
+        ui: uiSlice.reducer
+    }
+
+
+
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
