e83b81c4c34b4ec451d37b5cf6cd5980adae43ae
codingjhames <jhames.mejia@unipamplona.edu.co>
2022-09-06 12:21:44 -0500

    endpoints added

--- calendar-backend/routes/auth.js
+++ calendar-backend/routes/auth.js
@@ -1,20 +1,35 @@
 const { Router } = require('express');
 const router = Router();
 
+const { crearUsuario, loginUsuario, revalidarToken } = require('../controllers/auth');
+
 /* 
     Rutas de Usuarios / Auth
     host + /api/auth
 
 */
 
+router.post('/new', crearUsuario);
 
+router.post('/', loginUsuario );
 
-router.get('/', (req, res )  => {
+router.post('/renew', revalidarToken );
 
-    res.json({
-        ok:true
-    })
-});
+// router.post('/', (req, res )  => {
+
+//     res.json({
+//         ok:true,
+//         msg:'login'
+//     })
+// });
+
+// router.get('/renew', (req, res )  => {
+
+//     res.json({
+//         ok:true,
+//         msg:'renew'
+//     })
+// });
 
 
 module.exports = router;
