47a8589d9ab3843dc57bf107a4850e1e1e51213d
codingjhames <jhames.mejia@unipamplona.edu.co>
2022-09-06 10:04:36 -0500

    express backeend initialized

--- calendar-backend/index.js
+++ calendar-backend/index.js
@@ -1,2 +1,22 @@
 
-console.log('Hola Parchis');
+const express = require('express');
+
+// crear el servidor de express
+
+const app = express();
+
+// Rutas
+app.get('/', (req, res ) => {
+
+    // console.log('se requiere /');
+    res.json({
+        ok:true
+    })
+})
+
+
+
+// escuchar peticiones
+app.listen( 4000, () => {
+    console.log(`Servidor corriendo en puerto ${4000} `);
+} );
