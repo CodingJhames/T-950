7d83775137a1687424d476927fa467f00efc509e
codingjhames <jhames.mejia@unipamplona.edu.co>
2022-09-09 15:01:30 -0500

    spaces validations

--- calendar-backend/routes/events.js
+++ calendar-backend/routes/events.js
@@ -1,6 +1,9 @@
 const { Router } = require('express');
+const { check } = require('express-validator');
+const { fileValidator } = require('../middlewares/file-validator');
 const { getEvent, createEvent, updateEvent, deleteEvent } = require('../controllers/events');
 const { validateJWT } = require('../middlewares/validate-jwt');
+const { isDate } = require('../helpers/isDate');
 const router = Router();
 
 /* 
@@ -14,7 +17,15 @@ router.get('/', getEvent );
 
 /*-Crear un Nuevo evento */
 
-router.post('/', createEvent);
+router.post('/', 
+    [
+        check('title', 'El título es obligatorio').not().isEmpty(),
+        check('start', 'Fecha de incio es obligatoria').custom( isDate ),
+        check('end', 'Fecha de finalización es obligatoria').custom( isDate ),
+
+        fileValidator,
+    ],
+    createEvent);
 
 /*-Actualizar evento */
 
