9622a0bcce5e4bfdd67098ddb5af54d514749c44
codingjhames <jhames.mejia@unipamplona.edu.co>
2022-09-11 15:32:12 -0500

    update event

--- calendar-backend/controllers/events.js
+++ calendar-backend/controllers/events.js
@@ -41,12 +41,53 @@ const createEvent = async( req, res = response  ) => {
     }
 }; 
 
-const updateEvent = ( req, res = response  ) => {
+const updateEvent = async( req, res = response  ) => {
+
+    const eventId = req.params.id;
+    const uid = req.uid;
+
+    try {
+        
+        const event = await Event.findById(eventId);
+
+        if ( !event) {
+            res.status(404).json({
+                ok: false,
+                msg: 'Event does not exist'
+            });
+        }
+
+        if ( event.user.toString()  !== uid ) {
+            return res.status(401).json({
+                ok:false,
+                msg: 'No tiene privilegio de editar este evento'
+            });
+        }
+
+        const newEvent = {
+            ...req.body,
+            user:uid
+        }
+
+        const updatedEvent = await Event.findByIdAndUpdate( eventId, newEvent, { new: true } );
+        res.json({
+            ok: true,
+            event: updatedEvent
+        })
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({
+            ok:false,
+            msg: 'Contact Admin',
+        });
+    }
+
+
 
     res.json({
-        id:123456789,
         ok:true,
-        msg: 'updateEvent'
+        msg: 'updateEvent',
+        eventId
     })
 
 }; 
