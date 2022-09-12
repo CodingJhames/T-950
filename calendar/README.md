296cba66f3948ef3686c5eeff05be77bfa4355d7
codingjhames <jhames.mejia@unipamplona.edu.co>
2022-09-12 10:32:01 -0500

    environment vars created

---
+++ calendar/README.md
@@ -0,0 +1,13 @@
+
+# Calendar App
+
+
+## Development steps
+
+1. Rename the .env.template file to .env
+2. Do the respective changes to the environments variables.
+
+```
+    VITE_API_URL = http://localhost:4000/api
+
+```
--- calendar/src/router/AppRouter.jsx
+++ calendar/src/router/AppRouter.jsx
@@ -1,12 +1,14 @@
 import { Navigate, Route, Routes } from "react-router-dom"
 import { LoginPage } from "../auth";
 import { CalendarPage } from "../calendar/pages/CalendarPage";
+import { getEnvVariables } from "../helpers";
 
 
 export const AppRouter = () => {
 
     const authStatus = 'authenticated';
 
+    console.log( getEnvVariables());
 
   return (
     
