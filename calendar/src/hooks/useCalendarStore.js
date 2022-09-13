926dd4c9d635cd10f277f1efc38d65a80f8a7417
codingjhames <jhames.mejia@unipamplona.edu.co>
2022-09-13 15:20:53 -0500

    creating a new event

--- calendar/src/hooks/useCalendarStore.js
+++ calendar/src/hooks/useCalendarStore.js
@@ -1,5 +1,6 @@
 
 import { useDispatch, useSelector } from "react-redux"
+import { calendarApi } from "../api";
 import { onAddNewEvent, onDeleteEvent, onSetActiveEvent, onUpdateEvent } from "../store";
 
 
@@ -8,6 +9,8 @@ export const useCalendarStore = () => {
     const dispatch = useDispatch();
 
     const { events, activeEvent } = useSelector( state => state.calendar );
+    const { user } = useSelector( state => state.auth );
+
 
     const setActiveEvent = ( calendarEvent ) => {
         dispatch( onSetActiveEvent( calendarEvent )  );
@@ -15,10 +18,17 @@ export const useCalendarStore = () => {
 
     const startSavingEvent = async( calendarEvent ) => {
 
+        // ? TODO: Update Event
+
+
         if ( calendarEvent._id ) {
+            // ? actualizando
             dispatch( onUpdateEvent( {...calendarEvent} ) );
         } else {
-            dispatch(  onAddNewEvent( {...calendarEvent, _id: new Date().getTime() }) );
+            // ? Creando
+            const { data } = await calendarApi.post('/events', calendarEvent );
+            // console.log( {data} );
+            dispatch(  onAddNewEvent( {...calendarEvent, id: data.event.id, user }) );
         }
 
     }
